7eaa9520be44dfd5bb7168de2945bc27a4b596f8
Andrey Moskvin <andrey.moskvin@creuna.com>
2013-06-18 16:26:27 +0300

    Initial constraint holder test added

--- ParusTests/Specs/ConstraintContextSpec.m
+++ ParusTests/Specs/ConstraintContextSpec.m
@@ -9,12 +9,26 @@ describe(@"Constrinat Context", ^{
         context = [PVConstraintContext new];
     });
     
-    it(@"should raise an exception on empty state ", ^{
-        expect([context buildConstraint]).to.raiseAny();
-    });
-    
-    it(@"shuold generate correct constraint", ^{
-
+    it(@"should have constraint built", ^{
+        UIView* view1 = [UIView new];
+        UIView* view2 = [UIView new];
+        
+        context.leftView = view1;
+        context.leftAttribute = NSLayoutAttributeLeft;
+        context.rightView = view2;
+        context.rightAttribute = NSLayoutAttributeRight;
+        context.rightAtttributeMultiplier = 1.0f;
+        context.rightConstant = 10.0f;
+        context.priority = UILayoutPriorityDefaultLow;
+        
+        NSLayoutConstraint* constraint = [context buildConstraint];
+        expect(constraint.firstItem).to.equal(view1);
+        expect(constraint.firstAttribute).to.equal(NSLayoutAttributeLeft);
+        expect(constraint.secondItem).to.equal(view2);
+        expect(constraint.secondAttribute).to.equal(NSLayoutAttributeRight);
+        expect(constraint.multiplier).to.equal(1.0f);
+        expect(constraint.constant).to.equal(10.0f);
+        expect(constraint.priority).to.equal(UILayoutPriorityDefaultLow);
     });
 });
 
