dd5b0a7ca03950852a7d9cd5687a1f3db881198b
Kostya Bychkov <constantine.bychkov@gmail.com>
2015-05-17 23:07:36 +0300

    add frameworks schemes

--- Parus/src/Parus.h
+++ Parus/src/Parus.h
@@ -5,13 +5,15 @@
 //  Created by Alexey Demedetskiy on 21.08.13.
 //
 
+@import Foundation;
+
 //! Project version number for Parus iOS.
 FOUNDATION_EXPORT double ParusVersionNumber;
 
 //! Project version string for Parus iOS.
 FOUNDATION_EXPORT const unsigned char ParusVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Parus_iOS/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <Parus/PublicHeader.h>
 
 #import <Parus/PVLayout.h>
 #import <Parus/PVVFL.h>
